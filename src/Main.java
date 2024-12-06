a9df6597eefb007d963bf5afbce66a583d13fc7c
Daria <DariaProtos@yandex.ru>
2024-12-06 17:39:04 +0300

    Отредактировать код в соответствии с CodeStyle

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import controllers.InMemoryTaskManager;
-import model.Epic;
-import model.Status;
-import model.Subtask;
-import model.Task;
-
 public class Main {
 
     public static void main(String[] args) {
