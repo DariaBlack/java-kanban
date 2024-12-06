1bbb7b619be41457a3000c58eba73e41a542e918
Daria <DariaProtos@yandex.ru>
2024-12-06 14:33:38 +0300

    Создать исключение ManagerSaveException. Реализовать метод save()

--- src/controllers/FileBackedTaskManager.java
+++ src/controllers/FileBackedTaskManager.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import controllers.exceptions.ManagerSaveException;
 import controllers.interfaces.TaskManager;
 import model.Epic;
 import model.Subtask;
@@ -7,17 +8,48 @@ import model.Task;
 import java.io.*;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+    private final File file;
 
-    public FileBackedTaskManager() {
+    public FileBackedTaskManager(File file) {
+        this.file = file;
 
     }
 
     public void save() throws IOException {
+        try (Writer fileWriter = new FileWriter(file)) {
+            fileWriter.write("id,type,name,status,description,epic\n");
 
+            for (Task task : getTasks()) {
+                fileWriter.write( taskToString(task) + "\n");
+            }
+
+            for (Epic epic : getEpics()) {
+                fileWriter.write(epicToString(epic) + "\n");
+            }
+
+            for (Subtask subtask : getSubtasks()) {
+                fileWriter.write(subtaskToString(subtask)+ "\n");
+            }
+        } catch (RuntimeException e) {
+            throw new ManagerSaveException("Ошибка при попытке сохранения.");
+        }
+    }
+
+    private String taskToString(Task task) {
+        return task.getId() + ",TASK," + task.getName() + "," + task.getStatus() + "," + task.getDescription() + ",";
+    }
+
+    private String epicToString(Epic epic) {
+        return epic.getId() + ",TASK," + epic.getName() + "," + epic.getStatus() + "," + epic.getDescription() + ",";
+    }
+
+    private String subtaskToString(Subtask subtask) {
+        return subtask.getId() + ",TASK," + subtask.getName() + "," + subtask.getStatus() + "," + subtask.getDescription() + "," + subtask.getIdEpic();
     }
 
     static FileBackedTaskManager loadFromFile(File file) {
-
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file); // ещё что-то? чтение бакета?
+        return fileBackedTaskManager;
     }
 
     @Override
---
+++ src/controllers/exceptions/ManagerSaveException.java
@@ -0,0 +1,11 @@
+package controllers.exceptions;
+
+/*
+"Исключения вида IOException нужно отлавливать внутри метода save и выкидывать собственное непроверяемое исключение ManagerSaveException."
+Вот тут не совсем поняла, правильно ли я реализовала исключение. По заданию оно непроверяемое, а IOException из проверяемого типа
+ */
+public class ManagerSaveException extends RuntimeException {
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}
