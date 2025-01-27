58b7b89b7ee209af2249319de3bbd2164b7cc0ce
Daria <DariaProtos@yandex.ru>
2025-01-27 22:01:24 +0300

    Добавить реализацию классов SubtasksHandler и EpicsHandler

--- src/http/EpicsHandler.java
+++ src/http/EpicsHandler.java
@@ -1,4 +1,56 @@
 package http;
 
-public class EpicsHandler {
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import controllers.interfaces.TaskManager;
+import model.Epic;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class EpicsHandler extends BaseHttpHandler {
+    private final TaskManager taskManager;
+    private final Gson gson;
+
+    public EpicsHandler(TaskManager taskManager, Gson gson) {
+        this.taskManager = taskManager;
+        this.gson = gson;
+    }
+
+    private void handleGetEpics(HttpExchange exchange) throws IOException {
+        String response = gson.toJson(taskManager.getEpics());
+        sendText(exchange, response, 200);
+    }
+
+    private void handlePostEpic(HttpExchange exchange) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
+        Epic epic = gson.fromJson(inputStreamReader, Epic.class);
+        taskManager.addEpic(epic);
+        sendText(exchange, "Эпик успешно добавлен", 201);
+    }
+
+    private void handleDeleteEpics(HttpExchange exchange) throws IOException {
+        taskManager.deleteAllEpics();
+        sendText(exchange, "Все эпики успешно удалены", 200);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+
+        switch (method) {
+            case "GET":
+                handleGetEpics(exchange);
+                break;
+            case "POST":
+                handlePostEpic(exchange);
+                break;
+            case "DELETE":
+                handleDeleteEpics(exchange);
+                break;
+            default:
+                sendText(exchange, "Неизвестный метод", 405);
+        }
+    }
 }
--- src/http/SubtasksHandler.java
+++ src/http/SubtasksHandler.java
@@ -1,4 +1,56 @@
 package http;
 
-public class SubtasksHandler {
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import controllers.interfaces.TaskManager;
+import model.Subtask;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class SubtasksHandler extends BaseHttpHandler {
+    private final TaskManager tasksManager;
+    private final Gson gson;
+
+    public SubtasksHandler(TaskManager taskManager, Gson gson) {
+        this.tasksManager = taskManager;
+        this.gson = gson;
+    }
+
+    private void handleGetSubtasks(HttpExchange exchange) throws IOException {
+        String response = gson.toJson(tasksManager.getSubtasks());
+        sendText(exchange, response, 200);
+    }
+
+    private void handlePostSubtask(HttpExchange exchange) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
+        Subtask subtask = gson.fromJson(inputStreamReader, Subtask.class);
+        tasksManager.addSubtask(subtask);
+        sendText(exchange, "Подзадача успешно добавлена", 201);
+    }
+
+    private void handleDeleteSubtasks(HttpExchange exchange) throws IOException {
+        tasksManager.deleteAllSubtasks();
+        sendText(exchange, "Все подзадачи успешно удалены", 200);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+
+        switch (method) {
+            case "GET":
+                handleGetSubtasks(exchange);
+                break;
+            case "POST":
+                handlePostSubtask(exchange);
+                break;
+            case "DELETE":
+                handleDeleteSubtasks(exchange);
+                break;
+            default:
+                sendText(exchange, "Неизвестный метод", 405);
+        }
+    }
 }
