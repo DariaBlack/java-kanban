ae9daa8f2f032d7412146684409289b2f48fac79
Daria <DariaProtos@yandex.ru>
2025-01-28 04:11:56 +0300

    Исправить код в соответствии с CodeStyle

--- src/http/TasksHandler.java
+++ src/http/TasksHandler.java
@@ -9,7 +9,6 @@ import model.Task;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class TasksHandler extends BaseHttpHandler {
     private final TaskManager taskManager;
